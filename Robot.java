fcb8ec3334b39a3e3a33611e9771c46e942f9346
Pengy5934 <billy2000k@yahoo.com>
2018-10-29 18:51:51 -0400

    Add files via upload

---
+++ Robot.java
@@ -0,0 +1,10 @@
+//Billy Kelly
+//Robot
+
+public class Robot extends GriddedSprite
+{
+	public Robot(String name, Spritesheet s, Grid gr)
+	{
+		super (name, s, gr);
+	}
+}
---
+++ RobotDriver.java
@@ -0,0 +1,11 @@
+//Billy Kelly
+//RobotDriver
+//10.29.2018
+
+public class RobotDriver
+{
+	public static void main(String[] args)
+	{
+		RobotFrame rf = new RobotFrame();
+	}
+}
