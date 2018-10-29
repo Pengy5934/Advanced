88789bb24b910b1c8e91e927fc33050c3a063126
Pengy5934 <billy2000k@yahoo.com>
2018-10-29 18:54:15 -0400

    Update GriddedSprite.java

--- GriddedSprite.java
+++ GriddedSprite.java
@@ -6,21 +6,44 @@ import java.awt.*;
 
 public class GriddedSprite extends Sprite
 {
-    //GLOBAL VARIABLES
     private Grid gr;
+    private int row, col;
 
-	public GriddedSprite(String name, Spritesheet ss, Grid g)
+	public GriddedSprite(String name, Spritesheet ss, Grid g, int row, int col)
     {
 		super(name, ss);
 		gr = g;
+		this.row = row;
+		this.col = col;
     }
-
-    public void draw(Graphics g)
+   
+    //----------------------------------------------<MOVEMENT>-----------------------------------------------
+    public void moveUp()
     {
-
+		Box[][] boxes = gr.getBoxArray();
+		boxes[row][col].removeSprite(super.getName());
+		boxes[row--][col].addSprite(this);
 	}
-
+	
 	public void moveRight()
 	{
+		Box[][] boxes = gr.getBoxArray();
+		boxes[row][col].removeSprite(super.getName());
+		boxes[row][col++].addSprite(this);
 	}
+	
+	public void moveLeft()
+	{
+		Box[][] boxes = gr.getBoxArray();
+		boxes[row][col].removeSprite(super.getName());
+		boxes[row][col--].addSprite(this);
+	}
+	
+	public void moveDown()
+	{
+		Box[][] boxes = gr.getBoxArray();
+		boxes[row][col].removeSprite(super.getName());
+		boxes[row++][col].addSprite(this);
+	}
+    
 }
